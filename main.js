6edb32cd7e39d975ff4023fe91abff3c58290800
Patrick Bower <bower.patrick@gmail.com>
2020-06-29 20:54:38 +0100

    crap

--- main.js
+++ main.js
@@ -1,51 +1,46 @@
 const settings = {
-  widgetElement: undefined,
-  zoomWidget: 'widget',
-  zoomImg: 'widget__zoom-img'
+  zoomWidget: 'widget-zoom'
 }
 
-const createZoomImgEle = (zoomImgEle) => {
+const createZoomEle = (zoomEle) => {
   // get image src from data attr in DOM element
-  const zoomImageUrl = zoomImgEle.dataset.zoomImg;
-  // create an image element in memory
-  const img = new Image();
-  // set image src to load image element in memory
-  img.src = zoomImageUrl;
-  // get original width from element in memory
-  const zoomImageWidth = img.width;
-  // get original height from element in memory
-  const zoomImageHeight = img.height;
+  const zoomImageUrl = zoomEle.dataset.zoomImgSrc;
   // create background image on DOM widget element
-  zoomImgEle.setAttribute('style',` 
-    background-image: url(${zoomImageUrl}); 
-    background-size: ${zoomImageWidth}px ${zoomImageHeight}px; 
-    background-position: 0 0;
-    `);
+  zoomEle.setAttribute('style',`background-image: url(${zoomImageUrl});`);
 }
 
 const hoverPosition = (event) => {
+  console.log(event.target);
+  // get coordinates
   const zoomImg = event.target.getBoundingClientRect();
-  // x position within image
+  // x position within image as %
   const pointerX = event.clientX - zoomImg.left;
-  // y position within image
+  const positionX = (pointerX / zoomImg.width) * 100;
+  // y position within image as %
   const pointerY = event.clientY - zoomImg.top;  
-
-  const positionX = Math.floor((pointerX / zoomImg.width) * 100);
-  const positionY = Math.floor((pointerY / zoomImg.height) * 100);
-
-  event.target.style.backgroundPosition = `${positionX}% ${positionY}%`;
+  const positionY = (pointerY / zoomImg.height) * 100;
+  // use css transform origin for position
+  event.target.style.transformOrigin = `${positionX}% ${positionY}%`;
+  // use css transform scale for size
+  const scale = event.target.dataset.zoomImgScale;
+  event.target.style.transform = `scale(${scale})`; 
 }
 
-const addEvents = (zoomImgEle) => {
-  zoomImgEle.addEventListener('mousemove', hoverPosition);
+// const resetPosition = (event) => {
+//   event.target.style.transform = '';
+//   event.target.style.transformOrigin = '';
+// }
+
+const addEvents = (zoomEle) => {
+  zoomEle.addEventListener('mousemove', hoverPosition);
 }
 
 // Start when page has loaded
 window.addEventListener("load", () => {
   // get widget instance (if multiple loop to initialize each instance)
-  const zoomImgEle = document.querySelector(`.${settings.zoomImg}`);
+  const zoomEle = document.querySelector(`.${settings.zoomWidget}`);
   // setup zoom image
-  createZoomImgEle(zoomImgEle);
+  createZoomEle(zoomEle);
   // add events
-  addEvents(zoomImgEle);
+  addEvents(zoomEle);
 });
